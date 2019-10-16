34e0a6462c36d26517e59157ee8b8cd7f0b0f6ab
Keith Allleman <Keith.Alleman@icloud.com>
2019-10-16 12:06:13 -0600

    Trying to fix mobile layout on scoutCard.

--- src/components/_atoms/00-containers/MainContainerA.jsx
+++ src/components/_atoms/00-containers/MainContainerA.jsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 const Container = styled.main`
   width: 100%;
   height: 100%;
-  display: flex;
+  display: inline-flex;
   flex-flow: column;
   justify-content: space-evenly;
   align-items: center;
   padding: 3.750rem .313rem;
+  flex-shrink: 0;
 `;
 
 const MainContainerA = props => {
